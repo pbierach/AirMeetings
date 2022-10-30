2907f6a542ff906f7d5c0efb7f968839f9ba0eee
Pat Bierach <pbierach@ithaca.edu>
2022-10-30 18:47:49 -0400

    created models.py and setup db

---
+++ app/models.py
@@ -0,0 +1,100 @@
+from app import db
+
+
+# IMPLEMENT LOGIN, PASSWORD_HASHING
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64))
+    password_hash = db.Column(db.String(64))
+    name = db.Column(db.String(64))
+    guest = db.Column(db.Boolean)
+
+    def __repr__(self):
+        return '<User> {}'.format(self.name)
+
+
+class Location(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), index=True)
+    city = db.Column(db.String(64), index=True)
+    state = db.Column(db.String(64), index=True)
+    zip = db.Column(db.Integer)
+
+    def __repr__(self):
+        return '<Location> {}'.format(self.name)
+
+
+# CONSTRAINTS:
+# HOURLYRATE >= 0
+class Space(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), index=True)
+    sizeCap = db.Column(db.Integer, index=True)
+    hourlyRate = db.Column(db.Integer, index=True)
+    location = db.Column(db.Integer, db.ForeignKey('location.id'), index=True)
+
+    def __repr__(self):
+        return '<Location> {}'.format(self.name)
+
+
+# CONSTRAINTS:
+# START_TIME > END_TIME
+class meetingHistory(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    uid = db.Column(db.Integer, db.ForeignKey('user.id'), index=True)
+    spid = db.Column(db.Integer, db.ForeignKey('space.id'), index=True)
+    date = db.Column(db.Date())
+    startTime = db.Column(db.Time())
+    endTime = db.Column(db.Time())
+
+    def __repr__(self):
+        return '<meeting> {}'.format(self.spid + " on "
+                                     + self.date + " starting at"
+                                     + self.startTime)
+
+
+# CONSTRAINTS:
+# START_TIME > END_TIME
+class upcomingMeeting(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    uid = db.Column(db.Integer, db.ForeignKey('user.id'), index=True)
+    spid = db.Column(db.Integer, db.ForeignKey('space.id'), index=True)
+    date = db.Column(db.Date())
+    startTime = db.Column(db.Time())
+    endTime = db.Column(db.Time())
+
+    def __repr__(self):
+        return '<meeting> {}'.format(self.spid + " on "
+                                     + self.date + " starting at"
+                                     + self.startTime)
+
+
+# CONSTRAINTS:
+# SCORE >= 1
+class reviews(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    uid = db.Column(db.Integer, db.ForeignKey('user.id'))
+    spid = db.Column(db.Integer, db.ForeignKey('space.id'))
+    score = db.Column(db.Integer)
+
+    def __repr__(self):
+        return '<review> {}'.format(self.uid + " rated "
+                                    + self.spid + " "
+                                    + self.score)
+
+
+class Tech(db.Model):
+    id = db.Column(db.Integer, primary_key=True, index=True)
+    name = db.Column(db.String(64), index=True)
+
+    def __repr__(self):
+        return '<tech> {}'.format(self.name)
+
+
+# CONSTRAINTS:
+# count >= 1
+class TechToSpace(db.Model):
+    id = db.Column(db.Integer, primary_key=True, index=True)
+    tid = db.Column(db.Integer, db.ForeignKey('tech.id'))
+    spid = db.Column(db.Integer, db.ForeignKey('space.id'))
+    count = db.Column(db.Integer)
