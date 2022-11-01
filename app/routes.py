2af5259a17abe163cac329fd2c5a77101dcca47f
jguidi1 <jguidi@ithaca.edu>
2022-11-01 13:35:36 -0400

    populate DB Done

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from _curses import flash
 from datetime import date
 
-from flask import render_template
+from flamsk import render_template
 
 from app import app, db
 from app.models import User, Location, Space, meetingHistory, upcomingMeeting, reviews, Tech, TechToSpace
@@ -108,30 +108,121 @@ def populate_db():
     db.session.commit()
 
     UM1 = upcomingMeeting(uid=1,
-                         spid=S3.id,
-                         date=date(2023, 10, 10),
-                         startTime="10:30",
-                         endTime="11:30")
+                          spid=S3.id,
+                          date=date(2023, 10, 10),
+                          startTime="10:30",
+                          endTime="11:30")
 
     UM2 = upcomingMeeting(uid=2,
-                         spid=S4.id,
-                         date=date(2023, 11, 12),
-                         startTime="10:30",
-                         endTime="11:30")
+                          spid=S4.id,
+                          date=date(2023, 11, 12),
+                          startTime="10:30",
+                          endTime="11:30")
 
     UM3 = upcomingMeeting(uid=2,
-                         spid=S3.id,
-                         date=date(2025, 4, 8),
-                         startTime="9:00",
-                         endTime="11:00")
+                          spid=S3.id,
+                          date=date(2025, 4, 8),
+                          startTime="9:00",
+                          endTime="11:00")
 
     UM4 = upcomingMeeting(uid=3,
-                         spid=S3.id,
-                         date=date(2014, 9, 9),
-                         startTime="12:45",
-                         endTime="1:45")
+                          spid=S3.id,
+                          date=date(2014, 9, 9),
+                          startTime="12:45",
+                          endTime="1:45")
 
     db.session.add_all([UM1, UM2, UM3, UM4])
+    db.session.commit()
+
+    r1 = reviews(uid=1,
+                 spid=S3.id,
+                 score=4)
+    r2 = reviews(uid=1,
+                 spid=S4.id,
+                 score=5)
+    r3 = reviews(uid=2,
+                 spid=S6.id,
+                 score=4)
+    r4 = reviews(uid=3,
+                 spid=S4.id,
+                 score=2)
+
+    db.session.add_all([r1, r2, r3, r4])
+    db.session.commit()
+
+    t1 = Tech(name="Projector")
+    t2 = Tech(name="Wall Computers")
+    t3 = Tech(name="Desks")
+    t4 = Tech(name="Chairs")
+
+    db.session.add_all([t1, t2, t3, t4])
+    db.session.commit()
+
+    TS1 = TechToSpace(Tid=t1.id,
+                      spid=S2.id,
+                      count=1)
+
+    TS2 = TechToSpace(Tid=t2.id,
+                      spid=S2.id,
+                      count=8)
+
+    TS3 = TechToSpace(Tid=t3.id,
+                      spid=S2.id,
+                      count=10)
+
+    TS4 = TechToSpace(Tid=t4.id,
+                      spid=S2.id,
+                      count=20)
+
+    TS5 = TechToSpace(Tid=t3.id,
+                      spid=S4.id,
+                      count=25)
+
+    TS6 = TechToSpace(Tid=t4.id,
+                      spid=S4.id,
+                      count=50)
+
+    TS7 = TechToSpace(Tid=t1.id,
+                      spid=S2.id,
+                      count=1)
+
+    TS8 = TechToSpace(Tid=t4.id,
+                      spid=S1.id,
+                      count=20)
+
+    TS9 = TechToSpace(Tid=t1.id,
+                      spid=S3.id,
+                      count=1)
+
+    TS10 = TechToSpace(Tid=t2.id,
+                       spid=S3.id,
+                       count=5)
+
+    TS11 = TechToSpace(Tid=t4.id,
+                       spid=S3.id,
+                       count=10)
+
+    TS12 = TechToSpace(Tid=t1.id,
+                       spid=S5.id,
+                       count=2)
+
+    TS13 = TechToSpace(Tid=t3.id,
+                       spid=S5.id,
+                       count=3)
+
+    TS14 = TechToSpace(Tid=t4.id,
+                       spid=S5.id,
+                       count=6)
+
+    TS15 = TechToSpace(Tid=t1.id,
+                       spid=S6.id,
+                       count=1)
+
+    TS16 = TechToSpace(Tid=t4.id,
+                       spid=S6.id,
+                       count=8)
+
+    db.session.add_all([TS1, TS2, TS3, TS4, TS5, TS6, TS7, TS8, TS9, TS10, TS11, TS12, TS13, TS14, TS15, TS16])
     db.session.commit()
 
     return render_template('index.html', title="Home")
