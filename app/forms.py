0fc3daebe7cfeb556ff1882dd106ae8d9fef1cdd
jguidi1 <jguidi@ithaca.edu>
2022-11-04 10:49:36 -0400

    forms start

---
+++ app/forms.py
@@ -0,0 +1,44 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, TextAreaField, DateField, SelectMultipleField, SelectField
+from wtforms.validators import DataRequired
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+
+from app.models import User, Location, Space, meetingHistory, upcomingMeeting, reviews, Tech, TechToSpace
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField(
+        'Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different username.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different email address.')
+
+
+class HomeSearch(FlaskForm):
+    zipcode = StringField('Zipcode', validators=[DataRequired()])
+    date = DateField('Date', format='%Y-%m-%d')
+    time = StringField('Time', validators=[DataRequired()])
+    cost = StringField('Cost', validators=[DataRequired()])
+    submit = SubmitField('Search')
+
