8e74364e1619f7ddbefffdb51a81a00c7c3a3cc1
Pat Bierach <pbierach@ithaca.edu>
2022-11-08 10:31:23 -0500

    users can login now

--- app/models.py
+++ app/models.py
@@ -7,12 +7,19 @@ from werkzeug.security import generate_password_hash, check_password_hash
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64))
+    email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(64))
     name = db.Column(db.String(64))
     guest = db.Column(db.Boolean)
 
     def __repr__(self):
         return '<User> {}'.format(self.name)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 @login.user_loader
 def load_user(id):
