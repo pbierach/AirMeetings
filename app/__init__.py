0786868504adfe0e9c1331122f8f1a592e2e77c5
jguidi1 <jguidi@ithaca.edu>
2022-11-29 23:44:20 -0500

    css navbar

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
